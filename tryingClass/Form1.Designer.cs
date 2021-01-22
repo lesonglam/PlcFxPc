68d981744dfcc6b3b614962163bf31674980fe9a
lesonglam <lethanhslprv@gmail.com>
2021-01-22 09:45:50 +0700

    Signed-off-by: lesonglam <lethanhslprv@gmail.com>

---
+++ tryingClass/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace tryingClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxw1 = new System.Windows.Forms.TextBox();
+            this.textBoxw2 = new System.Windows.Forms.TextBox();
+            this.textBoxw3 = new System.Windows.Forms.TextBox();
+            this.textBoxw4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 14);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 180);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBoxw1
+            // 
+            this.textBoxw1.Location = new System.Drawing.Point(187, 14);
+            this.textBoxw1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBoxw1.Name = "textBoxw1";
+            this.textBoxw1.Size = new System.Drawing.Size(109, 34);
+            this.textBoxw1.TabIndex = 2;
+            this.textBoxw1.Text = "0";
+            this.textBoxw1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxw2
+            // 
+            this.textBoxw2.Location = new System.Drawing.Point(187, 58);
+            this.textBoxw2.Margin = new System.Windows.Forms.Padding(5);
+            this.textBoxw2.Name = "textBoxw2";
+            this.textBoxw2.Size = new System.Drawing.Size(109, 34);
+            this.textBoxw2.TabIndex = 3;
+            this.textBoxw2.Text = "0";
+            this.textBoxw2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxw3
+            // 
+            this.textBoxw3.Location = new System.Drawing.Point(187, 102);
+            this.textBoxw3.Margin = new System.Windows.Forms.Padding(5);
+            this.textBoxw3.Name = "textBoxw3";
+            this.textBoxw3.Size = new System.Drawing.Size(109, 34);
+            this.textBoxw3.TabIndex = 4;
+            this.textBoxw3.Text = "0";
+            this.textBoxw3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxw4
+            // 
+            this.textBoxw4.Location = new System.Drawing.Point(187, 146);
+            this.textBoxw4.Margin = new System.Windows.Forms.Padding(5);
+            this.textBoxw4.Name = "textBoxw4";
+            this.textBoxw4.Size = new System.Drawing.Size(109, 34);
+            this.textBoxw4.TabIndex = 5;
+            this.textBoxw4.Text = "0";
+            this.textBoxw4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(184, 190);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 62);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Write";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 288);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBoxw4);
+            this.Controls.Add(this.textBoxw3);
+            this.Controls.Add(this.textBoxw2);
+            this.Controls.Add(this.textBoxw1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxw1;
+        private System.Windows.Forms.TextBox textBoxw2;
+        private System.Windows.Forms.TextBox textBoxw3;
+        private System.Windows.Forms.TextBox textBoxw4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ tryingClass/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConnectPLCFX;
+using System.IO.Ports;
+
+namespace tryingClass
+{
+    public partial class Form1 : Form
+    {
+        ConnectPLCFX.ConnectPLCFX  connectPLCFX;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] w = new int[4];
+            w[0] = int.Parse(textBoxw1.Text);
+            w[1] = int.Parse(textBoxw2.Text);
+            w[2] = int.Parse(textBoxw3.Text);
+            w[3] = int.Parse(textBoxw4.Text);
+            
+            connectPLCFX.Write(w);
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          connectPLCFX = new ConnectPLCFX.ConnectPLCFX("COM1", 9600, 7, StopBits.One , Parity.Even);
+          connectPLCFX.Connect();
+          timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            int[] r = new int[4];
+            r = connectPLCFX.Read();
+            if (r != null)
+            {
+                textBox1.Text = r[0].ToString() + "\r\n";
+                textBox1.Text += r[1].ToString() + "\r\n";
+                textBox1.Text += r[2].ToString() + "\r\n";
+                textBox1.Text += r[3].ToString() + "\r\n";
+            }
+
+        }
+    }
+}
